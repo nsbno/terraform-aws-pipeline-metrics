3deb4b0cc723407291bc1b6df8e9dfba358c8aca
stekern <stekern@users.noreply.github.com>
2020-08-05 13:42:50 +0200

    Update label in CloudWatch dashboard

--- main.tf
+++ main.tf
@@ -218,7 +218,7 @@ resource "aws_cloudwatch_dashboard" "this" {
           height = 6
           properties = {
             metrics = [
-              [local.metric_namespace, "StateSuccess", "PipelineName", each.key, "StateName", state, { stat = "SampleCount", id = "m3", label = "Success" }]
+              [local.metric_namespace, "StateSuccess", "PipelineName", each.key, "StateName", state, { stat = "SampleCount", id = "m3", label = "Deployment Frequency" }]
             ]
             view     = "timeSeries"
             stacked  = false
